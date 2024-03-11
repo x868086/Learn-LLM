38c759c9a800334413d0286f05231ef1cb3856db
x868086 <43070810@qq.com>
2024-03-11 17:07:57 +0800

    init

--- 01/01.py
+++ 01/01.py
@@ -1,9 +1,15 @@
 import os
+
 from openai import OpenAI
 
 # 加载 .env 到环境变量
 from dotenv import load_dotenv, find_dotenv
+
 _ = load_dotenv(find_dotenv())
+
+# 获取环境变量加载openai模型
+model3 = os.environ.get("model3")
+model4 = os.environ.get("model4")
 
 # 配置 OpenAI 服务
 
@@ -13,11 +19,11 @@ response = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "请告诉我你是基于哪个模型训练出来的",
+            "content": "你是GPT3还是GPT4?",
         }
     ],
     # model="gpt-3.5-turbo",
-    model="gpt-4-1106-preview"
+    model=model4,
 )
 
 
@@ -28,4 +34,3 @@ response = client.chat.completions.create(
 
 # print(response)
 print(response.choices[0].message.content)
-
